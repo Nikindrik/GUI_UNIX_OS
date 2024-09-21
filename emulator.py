b0bab301878cc65d92b11472ede2ee07a2833dcb
Nick <68081589+Nikindrik@users.noreply.github.com>
2024-09-21 13:49:39 +0300

    v 0.0.2 added ZIP and update emulation UNIX

--- emulator.py
+++ emulator.py
@@ -6,43 +6,60 @@ import os
 
 files_list=[]
 
+def command_help():
+    console.text_list.append("List of commands:")
+    console.text_list.append(" help")
+    console.text_list.append(" ls")
+    console.text_list.append(" cd")
+    console.text_list.append(" exit")
+    console.text_list.append(" wc")
+    console.text_list.append(" mv")
+
+def command_clear():
+    console.text_list.clear()
+
+def command_ls():
+    files_list.clear()
+    items = os.listdir('.')
+    for item in items:
+        path = os.path.join('.', item)
+        if os.path.isfile(path):
+            files_list.append(path)
+        elif os.path.isdir(path):
+            files_list.append(path)
+    for i in range(len(files_list)):
+        console.text_list.append(files_list[i])
+
+def command_cd():
+    pass
+
+def command_wc():
+    pass
+
+def command_mv():
+    pass
+
+
 class Emulator():
     def __init__(self):
         pass
 
     def read_command(self, command):
         if command == "help":
-            console.text_list.append("List of commands:")
-            console.text_list.append(" help")
-            console.text_list.append(" ls")
-            console.text_list.append(" cd")
-            console.text_list.append(" exit")
-            console.text_list.append(" wc")
-            console.text_list.append(" mv")
+            command_help()
         elif command == "clear":
-            console.text_list.clear()
+            command_clear()
         elif command == "exit":
             exit()
         elif command == "ls":
-            files_list.clear()
-            items = os.listdir('.')
-            for item in items:
-                path = os.path.join('.', item)
-                if os.path.isfile(path):
-                    files_list.append(path)
-                elif os.path.isdir(path):
-                    files_list.append(path)
-            for i in range(len(files_list)):
-                console.text_list.append(files_list[i])
-        elif command == "help":
-            pass
+            command_ls()
         elif command == "cd":
-            pass
+            command_cd()
         elif command == "wc":
-            pass
+            command_wc()
         elif command == "mv":
-            pass
+            command_mv()
         else:
-            console.text_list.append("error")
+            console.text_list.append("ERROR")
         input_box.input_history.append(command)
         input_box.history_step = 0
